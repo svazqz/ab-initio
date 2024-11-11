759d588856811e4ee1743e56017ec4d8b5ad5354
Sergio Vazquez <sergio@incotechsystems.com>
2024-11-10 18:01:21 -0600

    README and LICENSE

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import path from "path";
 import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
 import { resolve } from "path";
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 const config = {
   'ab-initio': {
@@ -15,6 +16,10 @@ const config = {
   server: {
     entry: resolve(__dirname, './src/server.ts'),
     fileName: (format) => `server.${format}.js`,
+  },
+  'export-open-api': {
+    entry: resolve(__dirname, './src/open-api/index.ts'),
+    fileName: (format) => `open-api.${format}.js`,
   },
 };
 
@@ -35,37 +40,5 @@ export default defineConfig({
     //aiding in debugging.
     sourcemap: true,
   },
-  plugins: [dts()],
+  plugins: [dts(), nodePolyfills()],
 });
-
-/*
-export default defineConfig({
-  build: {
-    //Specifies that the output of the build will be a library.
-    lib: {
-      //Defines the entry point for the library build. It resolves 
-      //to src/index.ts,indicating that the library starts from this file.
-      entry: path.resolve(__dirname, "src/index.ts"),
-      name: "ab-initio",
-      //A function that generates the output file
-      //name for different formats during the build
-      fileName: (format) => `index.${format}.js`,
-    },
-    rollupOptions: {
-      external: [],
-      output: {
-        globals: {},
-      },
-    },
-    //Generates sourcemaps for the built files,
-    //aiding in debugging.
-    sourcemap: true,
-    //Clears the output directory before building.
-    emptyOutDir: true,
-  },
-  //react() enables React support.
-  //dts() generates TypeScript declaration files (*.d.ts)
-  //during the build.
-  plugins: [dts()],
-});
-*/
